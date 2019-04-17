a1458b11c9f3874931995724cd6822a351397aae
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-04-17 15:27:29 -0400

    initial code checkin

---
+++ src/components/select.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Select = ({ name, label, options, error, ...rest }) => {
+    return (
+        <div className="form-group">
+            <label htmlFor={name}>{label}</label>
+            <select name={name} id={name} {...rest} className="form-control">
+                <option value="0">--Please select a category ---</option>
+                {options.map(option => (
+                    <option key={option._id} value={option._id}>
+                        {option.type}
+                    </option>
+                ))}
+            </select>
+            {/* {error && <div className="alert alert-danger">{error} </div>} */}
+
+        </div>
+    );
+};
+
+export default Select;
