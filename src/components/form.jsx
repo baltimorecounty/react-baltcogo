d4679142ed9b1fddc625a6ba80c940c501c05763
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-04-26 10:49:44 -0400

    modified code

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -4,25 +4,29 @@ import Select from "./common/select";
 import TextArea from "./common/textarea";
 import Input from "./common/input";
 import FormReportType from './formreporttype'
-// import { getReportType } from './services/fakeReportType';
-// import { getSubCategoryType } from './services/fakeSubReportType';
-import PostData from './services/categories';
 import FormLocation from './formlocation'
+import PostData from './services/categories';
 import FormContact from './formcontact'
-
+import AppBar from './appbar'
 import Joi from "joi-browser";
-// import { reportType } from './services/fakeReportType';
 
+
+function getSteps() {
+  return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+}
 class Form extends Component {
 
   state = {
+    activeStep: 0,
+    completed: {},
     step: 1,
     isSubCategoryHidden: false,
     textareavalue: 'this is the value for text area',
-    data: {
-      typeId: "",
-      subtypeId: "",
-      ReqDesc: "",
+
+    dataReportType: {
+      categories: "",
+      subCategories: "",
+      description: "",
 
     },
     dataContact: {
@@ -38,33 +42,85 @@ class Form extends Component {
   };
 
 
-  getReportType() {
-    return PostData;
+  totalSteps = () => getSteps().length;
+
+
+  handleNext = () => {
+    let activeStep;
+
+    if (this.isLastStep() && !this.allStepsCompleted()) {
+      // It's the last step, but not all steps have been completed,
+      // find the first step that has been completed
+      const steps = getSteps();
+      activeStep = steps.findIndex((step, i) => !(i in this.state.completed));
+    } else {
+      activeStep = this.state.activeStep + 1;
+    }
+    this.setState({
+      activeStep,
+    });
+  };
+  completedSteps() {
+    return Object.keys(this.state.completed).length;
   }
-  getSubCategoryType(id) {
-    let t1 =  PostData.filter(m => m.id === parseInt(id)) ;
-    return t1[0].types
+
+  isLastStep() {
+    return this.state.activeStep === this.totalSteps() - 1;
+  }
+
+  allStepsCompleted() {
+    return this.completedSteps() === this.totalSteps();
   }
 
 
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1,
+    }));
+  };
+
+  handleStep = step => () => {
+    this.setState({
+      activeStep: step,
+    });
+  };
+
+  handleComplete = () => {
+    const { completed } = this.state;
+    completed[this.state.activeStep] = true;
+    this.setState({
+      completed,
+    });
+    this.handleNext();
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0,
+      completed: {},
+    });
+  };
+
+
+
+
+
+  //===========================================================================
   //Proceed to next step
   nextStep = () => {
     const { step } = this.state;
     this.setState({ step: step + 1 });
+    this.handleNext();
   }
   //Go back to pre step
   preStep = () => {
     const { step } = this.state;
     this.setState({ step: step - 1 });
-
+    this.handleBack();
   }
 
   buttonValidate = (data, schema) => {
-    console.log('--inside -- buttonValidate----');
-    console.log('-- data :');
-    console.log(data);
-    // console.log ('--schema--:data value:' + `this.state.${data}`);
-    console.log(schema);
+
 
     const options = { abortEarly: false };
     //const sdata = `this.state.${data}`;
@@ -88,7 +144,7 @@ class Form extends Component {
 
     const options = { abortEarly: false };
 
-    const { error } = Joi.validate(this.state.data, this.schema, options);
+    const { error } = Joi.validate(this.state.dataReportType, this.schema, options);
 
     console.log('error return:' + error);
 
@@ -106,11 +162,9 @@ class Form extends Component {
   validateProperty = ({ name, value }, schema) => {
 
     const obj = { [name]: value };
-    console.log('---name---');
-    console.log(name);
+
     const schema1 = { [name]: schema[name] };
-    console.log('---schema1---');
-    console.log(schema1);
+
     // const schema = { [name]: this.schema[name] };
     const { error } = Joi.validate(obj, schema1);
 
@@ -118,115 +172,86 @@ class Form extends Component {
 
   };
 
+  getReportType() {
+    return PostData;
+  }
 
-  handleChange = ({ currentTarget: input }, schema, data1) => {
-    // handleChange = ({ currentTarget: input }) => {
-    console.log('++++handleChange++++++');
-    console.log(input);
-    console.log('+++ out--handleChange++++');
+  getSubCategoryType(id) {
+    let t1 =  PostData.filter(m => m.id === parseInt(id)) ;
+    return t1[0].types
+  }
+
+
+  handleChange = ({ currentTarget: input }, schema) => {
 
     const errors = { ...this.state.errors };
-
     const errorMessage = this.validateProperty(input, schema);
     let data = {};
-    // data = { ...data };
+
     errors[input.name] = errorMessage;
-
-    //data[input.name] = input.value;
-
-
     let isSubCategoryHidden = '';
-
     if (errorMessage) {
-      if (input.name === 'typeId' || input.name === 'subtypeId') {
-        data = { ...this.state.data };
+      if (input.name === 'categories' || input.name === 'subCategories') {
+        data = { ...this.state.dataReportType };
         data[input.name] = input.value;
-        isSubCategoryHidden = input.name === 'typeId' ? false : true;
-
-        this.setState({ data, isSubCategoryHidden, errors });
-
+        isSubCategoryHidden = input.name === 'categories' ? false : true;
+        this.setState({ dataReportType: data, isSubCategoryHidden, errors });
       }
       else {
         data = { ...this.state.dataContact };
         data[input.name] = input.value;
         this.setState({ dataContact: data, errors });
-
       }
-
-
     }
-
     else {
-
       isSubCategoryHidden = true;
-
       delete errors[input.name];
-
-      if (input.name === 'typeId') {
- 
+      if (input.name === 'categories') {
         const reportsubcateories = this.getSubCategoryType(input.value);
-        data = { ...this.state.data };
+        data = { ...this.state.dataReportType };
         data[input.name] = input.value;
-        this.setState({ data, isSubCategoryHidden, reportsubcateories, errors });
-
+        this.setState({ dataReportType: data, isSubCategoryHidden, reportsubcateories, errors });
       }
-
-
-      else if (input.name === 'subtypeId') {
-        data = { ...this.state.data };
+      else if (input.name === 'subCategories') {
+        data = { ...this.state.dataReportType };
         data[input.name] = input.value;
-        this.setState({ data, isSubCategoryHidden, errors });
-
+        this.setState({ dataReportType: data, isSubCategoryHidden, errors });
       }
       else {
-        console.log('--input.name:' + input.name);
         data = { ...this.state.dataContact };
-        console.log('--input.value:' + input.value);
         data[input.name] = input.value;
         this.setState({ dataContact: data, errors });
-        console.log(data);
-        console.log('--input.name:---end');
       }
-
     }
-
   };
   handleChangeTextArea = ({ currentTarget: input }, schema) => {
-    console.log('inside change textarea ')
+
     const errors = { ...this.state.errors };
-    console.log(errors);
-    console.log("------input----------");
-    console.log(input);
-    console.log('--schema');
-    console.log(schema);
-    console.log("--end --input");
+
+
+
 
     const errorMessage = this.validateProperty(input, schema);
-    console.log(errorMessage);
-    console.log("++++");
+
     if (errorMessage) errors[input.name] = errorMessage;
 
     else delete errors[input.name];
 
-    const data = { ...this.state.data };
+    const data = { ...this.state.dataReportType };
 
     // console.log (input.name);
 
     data[input.name] = input.value;
-    console.log('----START ---input.value');
-    console.log(input.value);
-    console.log('----END ---input.value');
-    this.setState({ data, errors });
+
+    this.setState({ dataReportType: data, errors });
 
   };
 
   renderTextArea = (name, label, schema, type = "text") => {
-    console.log('--render text area---');
-    console.log(schema);
-    console.log('---end  render text area--');
-    const { data, errors } = this.state;
 
-    console.log(data);
+    const { dataReportType: data, errors } = this.state;
+
+    // console.log(data);
 
     return (
 
@@ -300,7 +325,7 @@ class Form extends Component {
 
   renderSelect = (name, label, options, schema) => {
 
-    const { data, errors } = this.state;
+    const { dataReportType: data, errors } = this.state;
     return (
 
       <Select
@@ -309,7 +334,7 @@ class Form extends Component {
         label={label}
         options={options}
         //onChange={e=> this.handleChange(e.currentTarget,schema)}
-        onChange={e => this.handleChange(e, schema, data)}
+        onChange={e => this.handleChange(e, schema)}
         // onChange={this.handleChange}
         error={errors[name]}
       />
@@ -320,12 +345,21 @@ class Form extends Component {
 
   render() {
 
-    const { step, reporttypes: values, isSubCategoryHidden, reportsubcateories, dataContact, dataReportType } = this.state;
+    const { step, activeStep, completed, reporttypes: values, isSubCategoryHidden, reportsubcateories, dataContact, dataReportType } = this.state;
 
     switch (step) {
+
       case 1:
-        return <FormReportType
-             nextStep={this.nextStep}
+        return (
+          <React.Fragment>
+        
+            <AppBar
+              activeStep={activeStep}
+              completed={completed}
+              handleStep={this.handleStep}
+            />
+            <FormReportType
+              nextStep={this.nextStep}
               handleChange={this.handleChange}
               handleNext={this.handleNext}
               renderSelect={this.renderSelect}
@@ -335,18 +369,37 @@ class Form extends Component {
               renderTextArea={this.renderTextArea}
               validate={this.buttonValidate}
               data={dataReportType} />
-      case 2:
-      return <FormLocation
-            nextStep={this.nextStep}
-            prestep={this.preStep} />
+          </React.Fragment>)
 
+
+      case 2:
+        return (
+          <React.Fragment>
+            <AppBar
+              activeStep={activeStep}
+              completed={completed}
+              handleStep={this.handleStep}
+            />
+            <FormLocation
+              nextStep={this.nextStep}
+              prestep={this.preStep} />
+          </React.Fragment>)
       case 3:
-        return<FormContact
-        nextStep={this.nextStep}
-        prestep={this.preStep}
-        renderInput={this.renderInput}
-        validate={this.buttonValidate}
-        data={dataContact} />
+        return (
+          <React.Fragment>
+            <AppBar
+              activeStep={activeStep}
+              completed={completed}
+              handleStep={this.handleStep}
+
+            />
+            <FormContact
+              nextStep={this.nextStep}
+              prestep={this.preStep}
+              renderInput={this.renderInput}
+              validate={this.buttonValidate}
+              data={dataContact} />
+          </React.Fragment>)
       default:
         return 'NONE'
     }
