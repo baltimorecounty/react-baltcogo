139880dbb18eeacfd652a56e5d3d98c077f31cf8
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-05-13 09:20:48 -0400

    formatted formcontainer logic

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 
-const FormContainer = props => <div className="container Container-bg"><h3>{props.title}</h3>{props.children}</div>
-
+const FormContainer = props => {
+	return (
+		<div className="container Container-bg">
+			<h3>{props.title}</h3>{props.children}
+		</div>
+	)
+};
 
 export default FormContainer
