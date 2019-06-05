c0506c8426f6605adddab874429f6d72067e194e
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-06-05 12:30:02 -0400

    code check in

--- src/components/authService.jsx
+++ src/components/authService.jsx
@@ -1,8 +1,8 @@
 import http from "./httpService";
 import { apiUrl } from "./config.json";
-const apiEndpoint = apiUrl + "/auth";
-
+const apiEndpoint = apiUrl + "/login";
+const proxyurl ="https://cors-anywhere.herokuapp.com/";  // thhis does not work 
 export function login(email, password) {
 
-    return http.post(apiEndpoint, { email, password });
+	return http.post(proxyurl + apiEndpoint, { email, password });
 }
