4ceea4c0a0ec4f3f3fab50070dfd23c3e32eeb8a
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-06-07 14:19:52 -0400

    merge

--- src/components/authService.jsx
+++ src/components/authService.jsx
@@ -1,6 +1,6 @@
 import http from "./httpService";
-import { apiUrl } from "./config.json";
-const apiEndpoint = apiUrl + "/login";
+import { apiLoginUrl } from "./config.json";
+const apiEndpoint = apiLoginUrl + "/login";
 export function login(email, password) {
 
 	return http.post(apiEndpoint, { email, password });
