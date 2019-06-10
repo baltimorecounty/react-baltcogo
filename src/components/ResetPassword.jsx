01d6ed08ca6984b9a2ab0de933b9d1d1c03101b6
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-06-10 15:11:12 -0400

    Created Password Reset. Connected Some End Points. Custom Error handler

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,23 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import ErrorMsg from "./ErrorMessage";
 import { ErrorCheck } from "./CustomErrorHandling";
 import FormContainer from './FormContainer';
-import { Login } from './authService';
+import { PasswordReset } from './authService';
 
 const SignIn = props => {
-	const [fieldType, setFieldType] = useState('Password');
-	const handlePasswordToggleChange = () => {
-		setFieldType(fieldType === 'Password' ? 'text' : 'Password');
-	};
-
 	const userLogin = async (values) => {
 
 		console.log('--inside signnup');
 		console.log(values);
 		try {
-			const response = await Login(values.Email, values.Password);
+			const response = await PasswordReset(values.Email);
 			if(response.data.ErrorsCount > 0){
 				console.log(ErrorCheck(response));
 			}
@@ -33,20 +28,13 @@ const SignIn = props => {
 
 	}
 	return (
-		<FormContainer title="Sign In">
+		<FormContainer title="Reset Password">
 			<Formik
 				initialValues={{
-					Email: '',
-					Password: ''
+					Email: ''
 				}}
 				validationSchema={Yup.object().shape({
 					Email: Yup.string().email('Invalid email address.').required('Please enter a valid email address.'),
-					//Password: Yup.string()
-					//	.required('Please enter your password.')
-					//	.max(30, "Maximum 30 characters allowed.")
-					//	.matches(
-					//		/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*(),.?":{}|<>]{8}/,
-					//		"Your password must be 8 to 30 characters and contain at least one uppercase letter, one lowercase letter and one number.")
 				})}
 
 				onSubmit={(values, { setSubmitting }) => {
@@ -74,30 +62,8 @@ const SignIn = props => {
 										errormessage={errors.Email}
 										touched={touched.Email} />
 								</div>
-								<div>
-									<label name="Password" htmlFor="password"
-										className={
-											errors.Password && touched.Password ? "input-feedback" : "text-label"}
-									>Password</label>
-									<Field
-										type={fieldType === 'Password' ? 'Password' : 'text'}
-										name="Password"
-										className={`text-input ${errors.Password && touched.Password ? "error" : ""}`}
-									/>
-									<span onClick={handlePasswordToggleChange}
-										className={`fa fa-fw fa-eye field-icon ${fieldType === 'text' ? "fa-eye-slash" : ""}`}></span>
-
-									<div className="input-feedback">
-										<ErrorMsg
-											errormessage={errors.Password}
-											touched={touched.Password} />
-									</div>
-								</div>
-								<label htmlFor="forgetpassword"> <a href="ResetPassword" >Forgot password?</a></label><br />
-								<label htmlFor="signup"
-								>Don't have an account? <a href="SignUpForm" >Sign up</a></label><br />
 								<button type="submit" disabled={isSubmitting}>
-									Sign In and Continue
+									Submit Reset Request
 								</button>
 							</Form>
 						)
--- src/components/authService.jsx
+++ src/components/authService.jsx
@@ -2,21 +2,26 @@ import http from "./httpService";
 import { apiLoginUrl, apiSignInUrl, apiPasswordResetUrl, apiCreateReportUrl } from "./config.json";
 const apiEndpointLogin = apiLoginUrl + "/login";
 const apiEndpointSignUp = apiSignInUrl + "/SignUp";
-const apiEndpointPasswordReset = apiPasswordResetUrl + "/PasswordReset";
+const apiEndpointPasswordReset = apiPasswordResetUrl + "/Password";
 const apiEndpointCreateReport = apiCreateReportUrl + "/CreateReport";
 //const proxyurl = "https://cors-anywhere.herokuapp.com/";  // thhis does not work 
-export function login(email, password) {
+export function Login(email, password) {
 	return http.post( apiEndpointLogin, { email, password });
 
 }
 export function SignUp(NameFirst, NameLast, Email, Password, Telephone, UniqueId, SuppressNotifications) {
-	return http.post(apiEndpointSignUp, { NameFirst, NameLast, Email, Password, Telephone, UniqueId, SuppressNotifications });
-
-	// TODO : use end point 
+	return http.post(apiEndpointSignUp, { 
+		NameFirst, 
+		NameLast, 
+		Email, 
+		Password, 
+		Telephone, 
+		UniqueId, 
+		SuppressNotifications });
 }
-export function PasswordReset() {
+export function PasswordReset(password) {
 
-	//TODO:use end point
+	return http.post( apiEndpointPasswordReset, { password });
 }
 
 export function CreateReport() {
