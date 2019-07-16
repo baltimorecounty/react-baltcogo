801e6e3adab9ae9adaa2248030235d7e5c6dede7
martypowell <jpowel25@gmail.com>
2019-07-16 08:02:53 -0400

    reformat

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,11 @@ import classNames from 'classnames';
 const Alert = (props) => {
 	const { type = '' } = props;
 	const cssClasses = classNames('alert', type ? `alert-${type}` : null);
-	return <div role="alert" className={cssClasses}>{props.children}</div>;
+	return (
+		<div role="alert" className={cssClasses}>
+			{props.children}
+		</div>
+	);
 };
 
 export default Alert;
