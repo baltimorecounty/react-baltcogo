1de168886b31722e2c44a2b672e29d55a0d62617
martypowell <jpowel25@gmail.com>
2019-07-18 09:01:49 -0400

    moved router to it's own file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
 import './App.css';
-import SignUpForm from './components/SignUpForm';
-import ServiceRequestForm from './components/ServiceRequestForm';
-import AdditionalInformationForm from './components/AdditionalInformation';
-import SignInForm from './components/SignInForm';
-import ResetPasswordForm from './components/ResetPasswordForm';
 import withFormik from './components/withFormik';
 import DisplayFormikState from './components/helper';
-import ProvideDetails from './components/ProvideDetails';
-import GetReport from './components/GetReport';
-import SubmitResponsePage from './components/SubmitResponsePage';
 import checkEnvironment from './utilities/returnEnvironmentItems';
-import { Routes } from './Routing';
+import { Router } from './Routing';
 
 function App(props) {
 	var Domain = checkEnvironment();
 	return (
-
 		<main className="container" >
-			<Switch>
-				<Route path={Routes.SignIn}
-					render={(routeProps) => <SignInForm {...routeProps}{...props} />} />
-				<Route path={Routes.SignUp}
-					render={(routeProps) => <SignUpForm {...routeProps}{...props} />} />
-				<Route path={Routes.ResetPassword}
-					render={(routeProps) => <ResetPasswordForm {...routeProps}{...props} />} />
-				<Route path={Routes.SubmitForm}
-					 render={(routeProps) => <SubmitResponsePage {...routeProps}{...props} />} />
-				<Route path={Routes.AdditionalInformation} component={AdditionalInformationForm} />
-				<Route path={Routes.ProvideDetails} component={ProvideDetails} />
-				<Route path={Routes.GetReport} component={GetReport} />
-				<Route path={Routes.Root} component={ServiceRequestForm} />
-			</Switch>
+			<Router {...props} />
 			{/* This is used for debugging purposes.  */}
 			{ Domain === "localhost:3000" ?  <DisplayFormikState {...props} /> : null}
 		</main>
--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,3 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import SignUpForm from './components/SignUpForm';
+import ServiceRequestForm from './components/ServiceRequestForm';
+import AdditionalInformationForm from './components/AdditionalInformation';
+import SignInForm from './components/SignInForm';
+import ResetPasswordForm from './components/ResetPasswordForm';
+import ProvideDetails from './components/ProvideDetails';
+import GetReport from './components/GetReport';
+import SubmitResponsePage from './components/SubmitResponsePage';
+
 const Routes = {
 	AdditionalInformation: '/AdditionalInformationForm',
 	GetReport: '/GetReport',
@@ -6,9 +17,20 @@ const Routes = {
 	Root: '/',
 	SignIn: '/SignInForm',
 	SignUp: '/SignUpForm',
-	SubmitForm: '/SubmitResponsePage',
+	SubmitForm: '/SubmitResponsePage'
 };
 
-export {
-	Routes
-};
+const Router = (props) => (
+	<Switch>
+		<Route path={Routes.SignIn} render={(routeProps) => <SignInForm {...routeProps} {...props} />} />
+		<Route path={Routes.SignUp} render={(routeProps) => <SignUpForm {...routeProps} {...props} />} />
+		<Route path={Routes.ResetPassword} render={(routeProps) => <ResetPasswordForm {...routeProps} {...props} />} />
+		<Route path={Routes.SubmitForm} render={(routeProps) => <SubmitResponsePage {...routeProps} {...props} />} />
+		<Route path={Routes.AdditionalInformation} component={AdditionalInformationForm} />
+		<Route path={Routes.ProvideDetails} component={ProvideDetails} />
+		<Route path={Routes.GetReport} component={GetReport} />
+		<Route path={Routes.Root} component={ServiceRequestForm} />
+	</Switch>
+);
+
+export { Routes, Router };
