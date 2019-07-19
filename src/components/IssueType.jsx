dd28ce539b923ac454dc7868fa2c572214449ead
martypowell <jpowel25@gmail.com>
2019-07-19 07:37:29 -0400

    added error handling for state and errors

--- src/components/IssueType.jsx
+++ src/components/IssueType.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-
-import { ErrorMessage } from "formik";
+import Alert from './Alert';
 import AutoCompleteTypeField from './AutocompleteTypeField';
-const IssueType = ({ rest, items, handleAddressChange, handleAddressSelect,  pageFieldName }) => {
+
+const IssueType = ({ name, formik = {}, items, handleAddressChange, handleAddressSelect,  pageFieldName }) => {
+	const { errors = {}, status = {}, values = {} } = formik;
+	const error = errors[name];
+	const statusMessage = status[name];
+	const errorMessage = error || statusMessage;
+
 	return (
 		<React.Fragment>
 			<div>
 				<label>Issue type</label>
 				<div>
-					<p className="smallest">{rest.formik.values.requestType} > {rest.formik.values.subRequestType}</p>
+					<p className="smallest">{values.requestType} > {values.subRequestType}</p>
 				</div>
 			</div>
 			<div className="address-search-wrapper">
@@ -18,18 +23,17 @@ const IssueType = ({ rest, items, handleAddressChange, handleAddressSelect,  pag
 				<div className="address-input-wrapper">
 					<AutoCompleteTypeField
 						items={items}
-						formikProps={rest}
-						value={rest.formik.values.location}
+						formik={formik}
+						value={values.location}
 						onChange={handleAddressChange}
 						onSelect={handleAddressSelect}
 					/>
 					<i className="fa fa-search address-search-icon" aria-hidden="true"></i>
 				</div>
 			</div>
-			<ErrorMessage name='msg' className='input-feedback' component='div' />
-			<div className={rest.formik.values.ShowErrorMsg === 1 ? "cs-form-control error" : "cs-form-control"}>
-				{rest.formik.values.ShowErrorMsg === 1 ? rest.formik.errors.location : ''}
-			</div>
+			{errorMessage && <Alert>
+				{errorMessage}
+			</Alert>}
 
 		</React.Fragment>
 	);
