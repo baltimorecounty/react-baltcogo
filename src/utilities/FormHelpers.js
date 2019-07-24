664cb285055c92a78ddf5b373097ded560ecc408
martypowell <jpowel25@gmail.com>
2019-07-24 13:56:08 -0400

    removed manual checks

--- src/utilities/FormHelpers.js
+++ src/utilities/FormHelpers.js
@@ -8,28 +8,7 @@ export const IsFormInComplete = (props) => {
 
 	if (requestType !== ""
 		&& subRequestType !== "") {
-		if (requestType === returnRequestTypes("requestType_RoadsAndSidewalks").toLowerCase()
-			&& subRequestType === returnRequestTypes("subCategory_IcyConditions").toLowerCase()) {
-			return true;
-		}
-		else if ((requestType === returnRequestTypes("requestType_WaterandSewerIssues").toLowerCase())
-			&& (subRequestType === returnRequestTypes("subCategory_SewerIssues").toLowerCase() ||
-				subRequestType === returnRequestTypes("subCategory_StormWaterIssues").toLowerCase() ||
-				subRequestType === returnRequestTypes("subCategory_WaterSupplyIssues").toLowerCase()
-			)) {
-			return true;
-		}
-		else if ((requestType === returnRequestTypes("requestType_TrashRecycleIssue").toLowerCase())
-			&& (subRequestType === returnRequestTypes("subCategory_CanOrLidLostDamaged").toLowerCase() ||
-				subRequestType === returnRequestTypes("subCategory_PropertyDamangeDuringCollecttion").toLowerCase() ||
-				subRequestType === returnRequestTypes("subCategory_RecyclingNotCollected").toLowerCase() ||
-				subRequestType === returnRequestTypes("subCategory_RequestToStartNewCollection").toLowerCase() ||
-				subRequestType === returnRequestTypes("subCategory_TrashNotCollected").toLowerCase() ||
-				subRequestType === returnRequestTypes("subCategory_YardWasteNotCollected").toLowerCase()
-			)) {
-			return true;
-		}
-		else if (requestType === returnRequestTypes("requestType_petAndAnimalIssue").toLowerCase()
+		if (requestType === returnRequestTypes("requestType_petAndAnimalIssue").toLowerCase()
 			&& subRequestType !== ""
 			&& props.values['petType'] === "") {
 			return true;
