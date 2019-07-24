6b39583409e1e1d4d35c5d6f6ccfda0f4cfe1684
martypowell <jpowel25@gmail.com>
2019-07-24 11:29:40 -0400

    reformat

--- src/components/petType.jsx
+++ src/components/petType.jsx
@@ -1,36 +1,45 @@
-import React from "react";
-import RequestPetTypeField from "./RequestPetTypeField";
-import ErrorMsg from "./ErrorMessage";
-const PetType = ({ requestType, requestType_petAndAnimalIssue, subRequestType, errorsPetType, touchedPetType, pageFieldName, handleServicePetChange, rest, PetTypes }) => {
-
+import React from 'react';
+import RequestPetTypeField from './RequestPetTypeField';
+import ErrorMsg from './ErrorMessage';
+const PetType = ({
+	requestType,
+	requestType_petAndAnimalIssue,
+	subRequestType,
+	errorsPetType,
+	touchedPetType,
+	pageFieldName,
+	handleServicePetChange,
+	rest,
+	PetTypes
+}) => {
 	return (
 		<React.Fragment>
-			{requestType === requestType_petAndAnimalIssue && subRequestType !== '' ?
-				<div className={
-					errorsPetType && touchedPetType ? "cs-form-control error" : "cs-form-control"}>
+			{requestType === requestType_petAndAnimalIssue && subRequestType !== '' ? (
+				<div className={errorsPetType && touchedPetType ? 'cs-form-control error' : 'cs-form-control'}>
 					<label htmlFor="petType">{pageFieldName}</label>
 					<RequestPetTypeField
 						component="select"
 						name="petType"
 						formikProps={rest}
 						onChange={handleServicePetChange}
-						className={errorsPetType && touchedPetType ? "text-select error" : null}
+						className={errorsPetType && touchedPetType ? 'text-select error' : null}
 					>
-						<option key='default' value=''>-- Please select a pet type --</option>
-						{PetTypes.map(petType => (
-							<option key={petType.id} value={petType.name}>{petType.name}</option>
+						<option key="default" value="">
+							-- Please select a pet type --
+						</option>
+						{PetTypes.map((petType) => (
+							<option key={petType.id} value={petType.name}>
+								{petType.name}
+							</option>
 						))}
 					</RequestPetTypeField>
-					<p role='alert' className="error-message">
-						{<ErrorMsg
-							errormessage={errorsPetType}
-							touched={touchedPetType} />}
+					<p role="alert" className="error-message">
+						{<ErrorMsg errormessage={errorsPetType} touched={touchedPetType} />}
 					</p>
 				</div>
-				: null}
+			) : null}
 		</React.Fragment>
 	);
 };
 
 export default PetType;
-
