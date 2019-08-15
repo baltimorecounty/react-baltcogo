63e195b72cd7e3708c0a2553f3f9fb5c03b8b531
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-08-15 09:27:07 -0400

    removed else

--- src/utilities/AlertHelper.jsx
+++ src/utilities/AlertHelper.jsx
@@ -7,12 +7,11 @@ export const getAlertMessage = (props) => {
 	if (hasPasswordReset) {
 		return <Note className='bc_alert alert-success'>{props.values.SignInPage.ResetPasswordAlert.replace('{email address}', props.history.location.state)}</Note>
 	}
-	else
-	{
-		const { incorrectEmail, networkError } = props.status || {};
-		const message = incorrectEmail || networkError || null;
-		return <Note icon='Nothing' className='bc_alert alert-warning'>{message}</Note>
-	}
+
+	const { incorrectEmail, networkError } = props.status || {};
+	const message = incorrectEmail || networkError || null;
+	return <Note icon='Nothing' className='bc_alert alert-warning'>{message}</Note>
+
 };
 
 
