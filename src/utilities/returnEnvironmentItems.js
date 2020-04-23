11dfaca82c90b285350bc36c49ffe125a5de5bd0
martypowell <jpowel25@gmail.com>
2020-04-23 19:33:35 -0400

    reformatted file

--- src/utilities/returnEnvironmentItems.js
+++ src/utilities/returnEnvironmentItems.js
@@ -1,25 +1,29 @@
-import * as configItems from './config'
-import _ from 'lodash';
+import * as configItems from "./config";
+import _ from "lodash";
 
 export const checkEnvironment = () => {
-	const environment = window.location.href;
-	var urlParts = environment.replace('http://','').replace('https://','').split(/[/?#]/);
-	return urlParts[0];
-}
+  const environment = window.location.href;
+  var urlParts = environment
+    .replace("http://", "")
+    .replace("https://", "")
+    .split(/[/?#]/);
+  return urlParts[0];
+};
 
 export const returnConfigItems = (configItemName, item) => {
-	var domain =  checkEnvironment();
-	var configItem = "";
+  var domain = checkEnvironment();
+  var configItem = "";
 
-	if (domain === "dev.baltimorecountymd.gov"){
-		configItem = _.filter(configItems[configItemName + 'Test'] , { key: item })
-	}else if(domain === "staging.baltimorecountymd.gov"){
-		configItem = _.filter(configItems[configItemName + 'Stage'], { key: item })
-	}else if(domain === "www.baltimorecountymd.gov"){
-		configItem = _.filter(configItems[configItemName + 'Prod'], { key: item })
-	}else{
-		configItem = _.filter(configItems[configItemName + 'Local'], { key: item })}
-	return configItem[0].value;
-}
+  if (domain === "dev.baltimorecountymd.gov") {
+    configItem = _.filter(configItems[configItemName + "Test"], { key: item });
+  } else if (domain === "staging.baltimorecountymd.gov") {
+    configItem = _.filter(configItems[configItemName + "Stage"], { key: item });
+  } else if (domain === "www.baltimorecountymd.gov") {
+    configItem = _.filter(configItems[configItemName + "Prod"], { key: item });
+  } else {
+    configItem = _.filter(configItems[configItemName + "Local"], { key: item });
+  }
+  return configItem[0].value;
+};
 
-export default checkEnvironment
+export default checkEnvironment;
