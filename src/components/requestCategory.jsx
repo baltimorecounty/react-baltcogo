4054c0f9e36cd9b98962bb4bcefab7bc2a5f95c9
sgurung <sgurung@OIT228469>
2020-05-01 14:10:49 -0400

    fix formatting

--- src/components/requestCategory.jsx
+++ src/components/requestCategory.jsx
@@ -4,73 +4,73 @@ import { Select } from "@baltimorecounty/dotgov-components";
 
 const RequestCategory = ({
   requestType,
-	errorsRequestType,
-	touchedRequestType,
-	pageFieldName,
-	handleServiceRequestChange,
-	rest,
-	Categories
+  errorsRequestType,
+  touchedRequestType,
+  pageFieldName,
+  handleServiceRequestChange,
+  rest,
+  Categories
 }) => {
-  const {requestTypeID} = rest.formik.values;
-	const handleChange = changeEvent => {
+  const { requestTypeID } = rest.formik.values;
+  const handleChange = changeEvent => {
     const localProps = rest.formik;
     const { name, options, selectedIndex } = changeEvent.target;
     const selectedText = options[selectedIndex].text.toLowerCase();
 
-		selectedIndex > 0
-			? localProps.setFieldValue(name, selectedText)
-			: localProps.setFieldValue(name, "");
-		localProps.setFieldTouched(name, true);
-		localProps.setFieldValue("requestTypeDescriptionID", "");
-		localProps.setFieldValue("requestTypeAddressID", "");
-		localProps.setFieldValue("requestTypeCityID", "");
-		localProps.setFieldValue("requestTypeZipID", "");
-		localProps.setFieldValue("subRequestTypeDescriptionID", "");
-		localProps.setFieldValue("subRequestTypeAddressID", "");
-		localProps.setFieldValue("subRequestTypeCityID", "");
-		localProps.setFieldValue("subRequestTypeZipID", "");
-		localProps.setFieldValue("subRequestType", "");
-		localProps.setFieldValue("subRequestTypeID", "");
-		localProps.setFieldValue("petType", "");
-		localProps.setFieldValue("petTypeID", "");
-		localProps.setFieldValue("otherAnimalTypes", "");
-		localProps.setFieldValue("otherAnimalTypesID", "");
-		localProps.setFieldValue("sexType", "");
-		localProps.setFieldValue("sexTypeID", "");
-		localProps.setFieldValue("animalColorType", "");
-		localProps.setFieldValue("animalColorTypeID", "");
-		localProps.setFieldValue("animalBreed", "");
-		localProps.setFieldValue("animalBreedID", "");
-		handleServiceRequestChange(changeEvent);
-	};
+    selectedIndex > 0
+      ? localProps.setFieldValue(name, selectedText)
+      : localProps.setFieldValue(name, "");
+    localProps.setFieldTouched(name, true);
+    localProps.setFieldValue("requestTypeDescriptionID", "");
+    localProps.setFieldValue("requestTypeAddressID", "");
+    localProps.setFieldValue("requestTypeCityID", "");
+    localProps.setFieldValue("requestTypeZipID", "");
+    localProps.setFieldValue("subRequestTypeDescriptionID", "");
+    localProps.setFieldValue("subRequestTypeAddressID", "");
+    localProps.setFieldValue("subRequestTypeCityID", "");
+    localProps.setFieldValue("subRequestTypeZipID", "");
+    localProps.setFieldValue("subRequestType", "");
+    localProps.setFieldValue("subRequestTypeID", "");
+    localProps.setFieldValue("petType", "");
+    localProps.setFieldValue("petTypeID", "");
+    localProps.setFieldValue("otherAnimalTypes", "");
+    localProps.setFieldValue("otherAnimalTypesID", "");
+    localProps.setFieldValue("sexType", "");
+    localProps.setFieldValue("sexTypeID", "");
+    localProps.setFieldValue("animalColorType", "");
+    localProps.setFieldValue("animalColorTypeID", "");
+    localProps.setFieldValue("animalBreed", "");
+    localProps.setFieldValue("animalBreedID", "");
+    handleServiceRequestChange(changeEvent);
+  };
 
-	return (
-		<React.Fragment>
-			<div
-				className={
-					errorsRequestType && touchedRequestType
-						? "cs-form-control error"
-						: "cs-form-control"
-				}
-			>
-				<Select
-					id="requestType"
-					label={pageFieldName}
-					name="requestType"
-					options={Categories}
-					onChange={handleChange}
-          value ={requestTypeID}
-					{...rest}
-				/>
-				<p role="alert" className="error-message">
-					<ErrorMsg
-						errormessage={errorsRequestType}
-						touched={touchedRequestType}
-					/>
-				</p>
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <div
+        className={
+          errorsRequestType && touchedRequestType
+            ? "cs-form-control error"
+            : "cs-form-control"
+        }
+      >
+        <Select
+          id="requestType"
+          label={pageFieldName}
+          name="requestType"
+          options={Categories}
+          onChange={handleChange}
+          value={requestTypeID}
+          {...rest}
+        />
+        <p role="alert" className="error-message">
+          <ErrorMsg
+            errormessage={errorsRequestType}
+            touched={touchedRequestType}
+          />
+        </p>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default RequestCategory;
