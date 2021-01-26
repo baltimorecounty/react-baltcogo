5a5ffeae210856fd437cb8c0d1c91caa2de7313b
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-01-26 12:01:17 -0500

    formatting

--- src/components/ServiceRequestForm.jsx
+++ src/components/ServiceRequestForm.jsx
@@ -35,7 +35,7 @@ const getUrlVars = () => {
   }
   return vars;
 };
-const getUrlVar = name => {
+const getUrlVar = (name) => {
   const searchParms = getUrlVars();
   return searchParms[name];
 };
@@ -44,50 +44,50 @@ const categoryId = getUrlVar("categoryid");
 
 const getSubCategories = (categories, categoryName) => {
   var category = categories.find(
-    category => category.name.toLowerCase() === categoryName
+    (category) => category.name.toLowerCase() === categoryName
   );
   return category ? category.types : [];
 };
 const getSubCategoriesIncludedDescription = (categories, categoryName) => {
   var subInfo = categories.find(
-    category => category.name.toLowerCase() === categoryName
+    (category) => category.name.toLowerCase() === categoryName
   );
   return subInfo ? subInfo : "";
 };
 const getIncludedDescriptions = (categories, categoryName) => {
   var category = categories.find(
-    category => category.name.toLowerCase() === categoryName
+    (category) => category.name.toLowerCase() === categoryName
   );
   return category ? category.description : "";
 };
 const getIncludedFields = (categories, categoryName) => {
   var category = categories.find(
-    category => category.name.toLowerCase() === categoryName
+    (category) => category.name.toLowerCase() === categoryName
   );
   return category ? category.fields : [];
 };
 const getshouldDisableForm = (subCategories, name) => {
   var type = subCategories.find(
-    subcategoryname => subcategoryname.name.toLowerCase() === name
+    (subcategoryname) => subcategoryname.name.toLowerCase() === name
   );
 
   return type && !!type.shouldDisableForm;
 };
 const getrequiresLocation = (categories, name) => {
   var category = categories.find(
-    category => category.name.toLowerCase() === name
+    (category) => category.name.toLowerCase() === name
   );
   return category ? category.requiresLocation : true;
 };
 const getAnimalSubCategories = (AnimalBreeds, animalName) => {
   var animalCats = AnimalBreeds.find(
-    animal => animal.animal.toLowerCase() === animalName
+    (animal) => animal.animal.toLowerCase() === animalName
   );
   return animalCats ? animalCats : [];
 };
 const getID = (categories, categoryName) => {
   var category = categories.find(
-    category => category.name.toLowerCase() === categoryName
+    (category) => category.name.toLowerCase() === categoryName
   );
   return category ? category.id : [];
 };
@@ -115,7 +115,7 @@ const ServiceRequestForm = (props, errors, touched) => {
     isPanelRequired,
     requestType,
     subRequestType,
-    petType
+    petType,
   } = localProps.values;
 
   const contactID =
@@ -188,7 +188,7 @@ const ServiceRequestForm = (props, errors, touched) => {
           ResetPasswordPage: resultFormFieldNames.data.ResetPasswordPage,
           ContactID: contactID,
           requestType: selectedType(),
-          subRequestType: selectedSubType()
+          subRequestType: selectedSubType(),
         };
 
         SetFieldValues(localProps, fields);
@@ -204,7 +204,7 @@ const ServiceRequestForm = (props, errors, touched) => {
     console.error("service request form data", ex);
   }
 
-  const SelectedValue = Categories => {
+  const SelectedValue = (Categories) => {
     return URLRouting(Categories, categoryId);
   };
 
@@ -223,7 +223,8 @@ const ServiceRequestForm = (props, errors, touched) => {
     const requestFields = {
       requestTypeID: ID,
       requestTypeDescriptionID: description,
-      requiresLocation: requiresLocation === undefined ? true : requiresLocation
+      requiresLocation:
+        requiresLocation === undefined ? true : requiresLocation,
     };
 
     SetFieldValues(localProps, requestFields);
@@ -232,7 +233,7 @@ const ServiceRequestForm = (props, errors, touched) => {
       const addressFields = {
         Latitude: MapDefaults.Latitude,
         Longitude: MapDefaults.Longitude,
-        location: MapDefaults.Location
+        location: MapDefaults.Location,
       };
       SetFieldValues(localProps, addressFields);
     }
@@ -241,7 +242,7 @@ const ServiceRequestForm = (props, errors, touched) => {
   };
 
   const addSelectedSubValueOptions = (Categories, value) => {
-    const subCategories = Categories.flatMap(x => x.types);
+    const subCategories = Categories.flatMap((x) => x.types);
     const subInfo = getSubCategoriesIncludedDescription(subCategories, value);
     let ID = getID(subCategories, value);
     const subCategory = GetSubCategory(Categories, ID);
@@ -260,7 +261,7 @@ const ServiceRequestForm = (props, errors, touched) => {
 
     const requestSubFields = {
       subRequestTypeID: ID,
-      shouldDisableForm: isDisabled
+      shouldDisableForm: isDisabled,
     };
 
     SetFieldValues(localProps, requestSubFields);
@@ -276,12 +277,12 @@ const ServiceRequestForm = (props, errors, touched) => {
 
       if (subInfo.description !== undefined) {
         SetFieldValues(localProps, {
-          requestTypeDescriptionID: subInfo.description
+          requestTypeDescriptionID: subInfo.description,
         });
       }
       if (subInfo.streetAddress !== undefined) {
         SetFieldValues(localProps, {
-          requestTypeAddressID: subInfo.streetAddress
+          requestTypeAddressID: subInfo.streetAddress,
         });
       }
       if (subInfo.city !== undefined) {
@@ -293,25 +294,25 @@ const ServiceRequestForm = (props, errors, touched) => {
     }
   };
 
-  const handleServiceRequestChange = changeEvent => {
+  const handleServiceRequestChange = (changeEvent) => {
     const { options, selectedIndex } = changeEvent.target;
     const selectedText = options[selectedIndex].text.toLowerCase();
     addSelectedValueOptions(Categories, selectedText);
   };
 
-  const handleServiceSubRequestChange = changeEvent => {
+  const handleServiceSubRequestChange = (changeEvent) => {
     const { options, selectedIndex } = changeEvent.target;
     const selectedText = options[selectedIndex].text.toLowerCase();
     addSelectedSubValueOptions(Categories, selectedText);
   };
 
-  const pullServiceRequestFields = fields => {
+  const pullServiceRequestFields = (fields) => {
     if (fields !== undefined) {
       const addressFields = {
         requestTypeAddressID: fields.streetAddress,
         requestTypeCityID: fields.city,
         requestTypeZipID: fields.zipCode,
-        isPanelRequired: true
+        isPanelRequired: true,
       };
       SetFieldValues(localProps, addressFields);
     } else {
@@ -319,7 +320,7 @@ const ServiceRequestForm = (props, errors, touched) => {
     }
   };
 
-  const handleServicePetChange = changeEvent => {
+  const handleServicePetChange = (changeEvent) => {
     const { options, selectedIndex } = changeEvent.target;
     const selectedText = options[selectedIndex].text.toLowerCase();
     let ID = getID(PetTypes, selectedText);
@@ -334,25 +335,25 @@ const ServiceRequestForm = (props, errors, touched) => {
     SetFieldValues(localProps, { [propertyName]: value });
   };
 
-  const handleAnimalColorChange = changeEvent => {
+  const handleAnimalColorChange = (changeEvent) => {
     handleFieldChange(changeEvent, "animalColorTypeID");
   };
 
-  const handleOtherPetTypeChange = changeEvent => {
+  const handleOtherPetTypeChange = (changeEvent) => {
     handleFieldChange(changeEvent, "otherAnimalTypesID");
   };
 
-  const handlePetSexChange = changeEvent => {
+  const handlePetSexChange = (changeEvent) => {
     handleFieldChange(changeEvent, "sexTypeID");
   };
 
-  const handleAnimalBreedChange = changeEvent => {
+  const handleAnimalBreedChange = (changeEvent) => {
     handleFieldChange(changeEvent, "animalBreedID");
   };
 
-  const checkPetType = value => {
+  const checkPetType = (value) => {
     value = value.toLowerCase();
-    var animalCats = AnimalBreeds.find(animal => animal.animal === value);
+    var animalCats = AnimalBreeds.find((animal) => animal.animal === value);
     if (animalCats !== undefined) {
       return true;
     } else {
@@ -385,7 +386,7 @@ const ServiceRequestForm = (props, errors, touched) => {
           NameFirst: NameFirst,
           NameLast: NameLast,
           Email: Email,
-          Telephone: Phone
+          Telephone: Phone,
         };
 
         SetFieldValues(localProps, fields);
@@ -412,7 +413,7 @@ const ServiceRequestForm = (props, errors, touched) => {
 
   const { values, isSubmitting, ...rest } = props;
 
-  const loadSelectedItems = props => {
+  const loadSelectedItems = (props) => {
     let requestType = localProps.values["requestType"];
 
     if (
